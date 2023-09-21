f882410f12275e0353a1c33a8d8dce108dc5bc23
Dionisio Miñano Jr <dionisiominano@gmail.com>
2023-09-21 15:55:58 +0800

    Third commit

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+// import encrypt from "mongoose-encryption";
+import md5 from "md5";
 // import _ from "lodash";
 
 const app = express();
+const mongodbPass = process.env.MONGODBPASS;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://Admin_Dionisio:AdminX123@cluster0.qe1zc6f.mongodb.net/userDB", { useNewUrlParser: true});
+// mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true});
+mongoose.connect(`mongodb+srv://${mongodbPass}@cluster0.qe1zc6f.mongodb.net/userDB`, { useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -21,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 //for encryption using mongoose-encryption & dotenv
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +43,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
@@ -54,7 +57,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
         .then((foundUser) => {
